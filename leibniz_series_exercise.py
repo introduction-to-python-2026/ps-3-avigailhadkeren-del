7ae95e88c67e64b700c53645e11e2a6894f79b32
avigailhadkeren-del <avigail.hadkeren@gmail.com>
2025-11-11 20:15:32 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,6 +1,6 @@
 def approximate_pi(n_terms):
     series sum = 0
-    for n = 1 to n_terms:
+    for n in range(n_terms):
         series_sum = series_sum + ((-1) ** n) / (2 * n + 1)
     
     approximate_pi = 4 * (1 - series_sum)
