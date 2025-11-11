b6580db09b041c55e5899bb8b482f9829a0bf2a0
avigailhadkeren-del <avigail.hadkeren@gmail.com>
2025-11-11 21:50:18 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,9 +1,6 @@
+def approximate_pi(n_terms):
     pi_estimate = 0
     for n in range(n_terms):
         pi_estimate += ((-1)**n) / (2*n + 1)
     pi_estimate *= 4
     return pi_estimate
-
-# Test the function
-for terms in [1, 10, 100, 1000, 10000]:
-    print(f"{terms} terms: {approximate_pi(terms)}") 
